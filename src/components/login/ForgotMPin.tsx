1d29dbb33c2d0d6f88d318d1ab26b5cbb389edcd
Nikhil Pannaswar <nikhilpannaswar2019@gmail.com>
2025-06-25 09:14:47 +0530

    fix: fixed captcha refresh button

--- src/components/login/ForgotMPin.tsx
+++ src/components/login/ForgotMPin.tsx
@@ -409,7 +409,7 @@ const ForgotMPin: React.FC<ForgotMPinProps> = ({
           <button
             type="button"
             onClick={refreshCaptcha}
-            className="p-2 rounded bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
+            className="p-3 rounded bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600"
           >
             <RefreshCw
               size={18}
